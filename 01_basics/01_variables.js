7070bfea39fbd979965cf5fce0cfa017ce265330
Anjali Tripathi <anjalitripathi.kit@gmail.com>
2025-05-16 19:17:52 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+ 
+ const accountId = 12255
+ let accountEmail = "Anjali@google.com"
+ var accountPassword = "12345"
+ accountCity = "Jaipur"
+ let accountState;
+
+
+
+ accountEmail = "Abc@.com"
+ accountPassword = "22222"
+ accountCity = "Bengluru"
+
+ 
+//  const accountId = 111345  change not allowed
+
+// prefer not to use var because of an issue of block scope and fuctional scope  
+
+ console.log(accountId);
+
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+ 
+ 
+ 
+ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+ "use strict";  // treat all Js code as newer version
+
+//  alert( 3 + 3);   We are using nodejs not browser 
+ 
+ console.log( 3 
+    +      //  readability should be high
+    3);  
+ 
+ console.log("Anjali")
+
+
+ let name = "Anjali"
+ let age = 18
+ let isloggedin = false
+
+ 
+ 
+ 
+
+ // primitive datatypes
+ 
+ // number 2 to power 53
+ // bigint => Big values
+ // string => ""
+ // boolean => true/false
+ // null => represents empty value  standlone value
+ // undefined 
+ // symbol => uniqueness
+
+
+ // object
+
+ console.log(typeof null); // object
+ console.log(typeof undefined);  // undefined
+ 
+ 
