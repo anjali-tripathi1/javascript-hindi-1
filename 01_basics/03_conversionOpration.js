55750c9e15f84c9e54d2c2427d8774a369299f36
Anjali Tripathi <anjalitripathi.kit@gmail.com>
2025-05-17 14:00:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,60 @@
+ 
+//  let score = 33
+ // now if write in string
+
+//  let score = "33" 
+ // suppose now number is write in number and alphabet too
+
+//  let score = "33abc"
+
+// if null is given
+// let score = null 
+
+// let score = undefined
+
+// if in boolean
+ // let score = true 
+
+//if string is given and value is not number
+ let score = "Anjali"
+
+ //console.log(typeof score)
+ //console.log(typeof(score));
+
+ // now we know that our value is in number 
+ let valueInNumber = Number(score)
+ //console.log(valueInNumber)
+
+// NaN stands for not a number it's also a type that can be checked that what is value.
+
+// if value is not converted so in null it is 0, in undefined it is NaN, in alphabet and number
+ // it is NaN  
+
+// Notes
+// "33" => 33
+// "33abc" => NaN
+// boolean => true => 1;, false => 0;
+
+// let isLoggedIn = 1 
+
+// if empty string
+// let isLoggedIn = "" 
+let isLoggedIn = "Anjali"
+
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//  1 => true, 0 => false
+// "" => false
+// "Anjali" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+ 
+
