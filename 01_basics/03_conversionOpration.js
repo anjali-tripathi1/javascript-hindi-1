c1247cbc585df07065c26690d16e869f5327544f
Anjali Tripathi <anjalitripathi.kit@gmail.com>
2025-05-20 18:09:42 +0000

    comparison

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -53,8 +53,74 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ###################### Operations #######################
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// Operation => it means addition, subtraction, divide, multiplication, remainder
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// tricky things
+
+let str1 = "Hello"
+let str2 = " Anjali"
+
+let str3 = str1 + str2
+// console.log(str3); 
+
+// problem is here
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + " 2");
+// console.log("1" + 2 + 2);
+
+// if write in first string then all conversion would be treat in string
+// console.log(1 + 2 + "2"); 
+
+// if write in last string then conversion will be done before values
+
+// console.log( (3 + 4 ) * 5 % 3); // this syntax shoulnd't use
+
+// console.log(( 3 + 5 ) * (2*3) / (16)); 
+
+// for this type of code we should use this type of strcture specially parenthesis
+
+// console.log(+true);
+// console.log(+"");
+
+// these should't use
+let num1, num2, num3                                
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// presidence oprator agar prefix karte hai to kya hota hai aur postfix karte hai to kya hota hai.
+// prefix ke andar values pehle incriment ho jati hai, aur postfix ke andar usage ke baad incriment hoti
+// hai.
+
 
  
+
+
+
+
+
+
+
 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+ 
+//  console.log( 2 > 1);
+//  console.log( 2 >= 1);
+//  console.log( 2 < 1);
+//  console.log( 2 == 1);
+//  console.log( 2 != 1);
+ 
+console.log( "2" > 1);
+console.log( "02" > 1);
+
+// automatically javascript 2 ko number mein convert kar dega.
+// Jab hum is tarah ka conversion karte hai to comparison predictable result nahi deta hai.
+// Jab bhi compare kare values to yeh ensure kare ke dono ka datatype same ho.
+
+// typescipt bhi language hai aur yeh allow hi nahi karta ki aap do diffrent datatypes lo compare karo.
+
